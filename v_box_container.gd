0f71882bd3e87cead1a7257b93ede4ce0c04eb96
vinibcc <marcos.isidorio@ufape.edu.br>
2026-03-08 19:12:40 -0300

    Script de botões de tela inicial e criação de telas de vitória e derrota.

--- v_box_container.gd
+++ v_box_container.gd
@@ -2,10 +2,22 @@ extends VBoxContainer
 
 func _ready():
 	$start.grab_focus()
+	
 	if not $start.pressed.is_connected(_on_start_pressed):
 		$start.pressed.connect(_on_start_pressed)
-
+		
+	if not $ranking.pressed.is_connected(_on_ranking_pressed):
+		$ranking.pressed.connect(_on_ranking_pressed)
+		
+	if not $sair.pressed.is_connected(_on_sair_pressed):
+		$sair.pressed.connect(_on_sair_pressed)
 
 func _on_start_pressed():
 	CampaignState.iniciar_nova_campanha()
 	get_tree().change_scene_to_file("res://scenes/modo_campanha.tscn")
+
+func _on_ranking_pressed():
+	get_tree().change_scene_to_file("res://scenes/tela_ranking.tscn")
+
+func _on_sair_pressed():
+	get_tree().quit()
